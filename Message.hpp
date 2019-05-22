eb839c40546a0358a8461a6c965cf6638cbe21a3
Timecur <45092718+Timecur@users.noreply.github.com>
2019-05-22 23:00:37 +0800

    Update Message.hpp

--- Message.hpp
+++ Message.hpp
@@ -2,76 +2,69 @@
 
 #include <iostream>
 #include <string>
-#include "json/json.h"
 #include "ProtocolUtil.hpp"
-
-using namespace std;
+#include "json/json.h"
 
 #define NORMAL_TYPE 0
 #define LOGIN_TYPE 1
 
 class Message{
-public:
-    string nick_name;
-    string school;
-    string text;
-    unsigned int id;
-    unsigned int type;
-
-public:
-
-    Message()
-    {}
-
-    Message(const string &_nick_name, const string &_school, const string _text,\
-            const unsigned int &_id, const unsigned int &_type = NORMAL_TYPE)
-        :nick_name(_nick_name)
-        ,school(_school)
-        ,text(_text)
-        ,id(_id)
-        ,type(_type)
-    {}
-
-    void ToSendString(std::string& sendString){
-        Json::Value root;
-        root["name"] = nick_name;
-        root["school"] = school;
-        root["text"] = text;
-        root["id"] = id;
-        root["type"] = type;
-        Util::Seralizer(root, sendString);
-    }
-
-    void ToRecvValue(string &recvString){
-        Json::Value root;
-        Util::UnSeralizer(recvString, root);
-        nick_name = root["name"].asString();
-        school = root["school"].asString();
-        text = root["text"].asString();
-        id = root["id"].asInt();
-        type = root["type"].asInt();
-    }
-
-    const string &NickName(){
-        return nick_name;
-    }
-
-    const string &School(){
-        return school;
-    }
-
-    const string &Text(){
-        return text;
-    }
-
-    const unsigned int &Id(){
-        return id;
-    }
-    
-    const unsigned int &Type(){
-        return type;
-    }
-
-    ~Message()
-    {}
+    private:
+        std::string nick_name;
+        std::string school;
+        std::string text;
+        unsigned int id;
+        unsigned int type;
+    public:
+        Message()
+        {}
+        Message(const std::string &n_, const std::string &s_,\
+                const std::string &t_, const unsigned int &id_,\
+                unsigned int type_ = NORMAL_TYPE)
+            :nick_name(n_), school(s_), text(t_), id(id_),type(type_)
+        {
+        }
+        void ToSendString(std::string &sendString)
+        {
+            Json::Value root;
+            root["name"] = nick_name;
+            root["school"] = school;
+            root["text"] = text;
+            root["id"] = id;
+            root["type"] = type;
+            Util::Seralizer(root, sendString);
+        }
+        void ToRecvValue(std::string &recvString)
+        {
+            Json::Value root;
+            Util::UnSeralizer(recvString, root);
+            nick_name = root["name"].asString();
+            school = root["school"].asString();
+            text = root["text"].asString();
+            id = root["id"].asInt();
+            type = root["type"].asInt();
+        }
+        const std::string &NickName()
+        {
+            return nick_name;
+        }
+        const std::string &School()
+        {
+            return school;
+        }
+        const std::string &Text()
+        {
+            return text;
+        }
+        const unsigned int &Id()
+        {
+            return id;
+        }
+        const unsigned int &Type()
+        {
+            return type;
+        }
+        ~Message()
+        {
+        }
 };
